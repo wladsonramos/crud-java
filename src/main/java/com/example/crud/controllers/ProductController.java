c9199c0660c3fcc6aba3a1919a3eeee8c33f041e
Wladson Ramos <wladsonramossi@gmail.com>
2023-12-03 09:10:08 -0300

    add exception handler

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.example.crud.controllers;
 import com.example.crud.domain.product.Product;
 import com.example.crud.domain.product.ProductRepository;
 import com.example.crud.domain.product.RequestProduct;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,7 +41,7 @@ public class ProductController {
             product.setPrice_in_cents(data.price_in_cents());
             return ResponseEntity.ok(product);
         } else {
-            return  ResponseEntity.notFound().build();
+            throw new EntityNotFoundException();
         }
     }
 
@@ -53,7 +54,7 @@ public class ProductController {
             product.setActive(false);
             return ResponseEntity.noContent().build();
         } else {
-            return  ResponseEntity.notFound().build();
+            throw new EntityNotFoundException();
         }
     }
 }
