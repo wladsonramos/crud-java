d96c7b7edaa3a5d5307e43cea6d53a99d5f1effa
Wladson Ramos <wladsonramossi@gmail.com>
2023-12-03 06:28:11 -0300

    add column active

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -19,7 +19,7 @@ public class ProductController {
     private ProductRepository repository;
     @GetMapping
     public ResponseEntity getAllProducts(){
-        var allProducts =  repository.findAll();
+        var allProducts =  repository.findAllByActiveTrue();
         return ResponseEntity.ok(allProducts);
     }
 
@@ -45,8 +45,15 @@ public class ProductController {
     }
 
     @DeleteMapping("/{id}")
+    @Transactional
     public ResponseEntity deleteProduct(@PathVariable String id){
-        repository.deleteById(id);
-        return ResponseEntity.noContent().build();
+        Optional<Product> optionalProduct = repository.findById(id);
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setActive(false);
+            return ResponseEntity.noContent().build();
+        } else {
+            return  ResponseEntity.notFound().build();
+        }
     }
 }
